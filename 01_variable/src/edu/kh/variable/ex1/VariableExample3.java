316c1a09299aa20728a56cd46666326483f2289d
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-02-01 17:12:47 +0900

    Day16 02/01

---
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample3.java
@@ -0,0 +1,16 @@
+package edu.kh.variable.ex1;
+
+public class VariableExample3 {
+
+	/** 형변환은 왜 필요할까?
+	 * 컴퓨터는 기본적으로 컴파일러
+	 *
+	 * */
+	
+	int num1 = 10;
+	double num2 = 3.5;
+	int i2 = 2_100;
+	char ch4 = '각';
+	int i4 = ch4;
+	System.out.println(i4);
+}
---
+++ 01_variable/src/edu/kh/variable/ex1/VariableExample5.java
@@ -0,0 +1,1 @@
+package edu.kh.variable.ex1;
