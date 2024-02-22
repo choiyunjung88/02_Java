09ddd287facc3f228aac2ea0299a130dc5e2bbd5
choiyunjung88 <lucy023@sookmyung.ac.kr>
2024-02-22 17:44:48 +0900

    Day28 02/22

--- 12_io/src/edu/kh/io/pack1/model/service/FileService.java
+++ 12_io/src/edu/kh/io/pack1/model/service/FileService.java
@@ -32,63 +32,70 @@ public class FileService {
 	 * File[]   listFiles()     : 디렉토리 내 파일 목록을 File[] 배열로 반환
 	 */
 
-	/*[개발자 상식]
+	
+	/* [개발자 상식!]
 	 * 
-	 * OS : 운영체제
+	 * OS : 운영 체제
 	 * 
-	 * (windows) / (linux, mac)
-	 *  폴더         디렉토리
-	 *  - 경로 표기 방법(하위 폴더, 파일 표시)
-	 *  백슬래시(\, Windows)
-	 *  슬래시 (/, linux, mac)
+	 *  (Windows)    /     (Linux, Mac)
+	 *    폴더       /      디렉토리
 	 *  
-	 *  java 언어의 특징 : 플랫폼에 독립적
-	 *  os 상관없이 똑같은 코드 작성이 가능
-	 *  - 이틀징을 유지하기 위해 \, / 호환가능
-	 *  
-	 *  //////////////////////////////////
-	 *  
-	 *  경로표기 방법
-	 *  1) 절대 경로 : 하나의 기준으로부터
-	 *               목표까지의 경로를 모두 표기하는 방법
-	 *  - 기준
-	 *  윈도우 c:/ , d:/
-	 *  리눅스 , 맥 : /users, /
-	 *  2) 상대 경로 : 현재 위치를 기준으로 목표까지의 경로를 표기하는 방법
-	 *  
-	 *  
-	 *  
-	 *   
+	 * - 경로 표기 방법(하위 폴더, 파일 표시)
+	 * 
+	 * 백슬래시 (\, Windows)     -> C:\workspace\02_Java
+	 * 슬래시   (/, Linux, Mac ) -> C:/workspace/02_Java
+	 * 
+	 * 
+	 * - Java 언어의 특징 : 플랫폼(OS)에 독립적
+	 * -> OS 관계 없이 똑같은 코드 작성이 가능
+	 * 
+	 * -> 이 특징을 유지하기 위해 /, \ 둘다 호환 가능
+	 *   (런타임 시 실행되는 OS에 맞게 자동으로 변경)
+	 * 
+	 * 
+	 * -------------------------------------------------------------
+	 * 
+	 * - 경로 표기 방법
+	 * 
+	 * 1) 절대 경로 : 하나의(절대적인) 기준으로 부터
+	 * 				 목표까지의 경로를 모두 표기하는 방법
+	 * 
+	 * - 기준
+	 * [Windows] :   C:/  , D:/
+	 * [Linux, Mac] : /Users ,   /
+	 * 
+	 * 
+	 * 2) 상대 경로 : 현재 위치를 기준으로 목표까지의 경로를 표기하는 방법
+	 * 
 	 * 
 	 * */
 	
 	
-	/**file 클래스로 객체 생성
-	 * 폴더가 존재하지 않으면 폴더 생성
-	 * 
+	/**
+	 * File 클래스로 객체 생성
+	 * + 폴더가 존재하지 않으면 폴더 생성
 	 */
 	public void method1() {
+		
+		// 제일 앞 "/" : 최상위 폴더 (== root, 절대 경로의 기준점)
+		
 		// C:/io_test/20240222 폴더를 지정
-		// 제일앞 / : 최상위 폴더 (==root, 절대 경로의 기준점)
 		File directory = new File("/io_test/20240222");
+		// -> 처음 : 존재하지 않는 폴더를 관리하는 File 객체 생성
 		
-		//-> 처음 : 존재하지 않는 폴더를 관리하는 file 객체 생성
+		System.out.println("존재 여부 확인 : " + directory.exists());
 		
-		System.out.println("존재여부확인:"+directory.exists());
-		
-		if(!directory.exists()) { // 폴더가 존재하지 않으면
+		if ( !directory.exists() ) { // 폴더가 존재하지 않으면
+			
 			// 폴더 모두 생성
 			directory.mkdirs();
+			
 			System.out.println(directory.getName()); // 20240222
 			System.out.println(directory.getPath()); // \io_test\20240222
-			
 		}
 		
-		
-		
-		
-		
 	}
+	
 	
 	
 	/**
@@ -96,73 +103,96 @@ public class FileService {
 	 * 지정된 위치에 파일 생성하기
 	 */
 	public void method2() {
-		// 문자열에서 "\" 표기하는 방법 : " \ \"
+		// 문자열에서 "\" 표기하는 방법 : "\\"
 		File file = new File("\\io_test\\20240222\\파일생성.txt");
-				
+		
 		try {
-			if (!file.exists()) {
+			
+			if( !file.exists() ) { // 존재하지 않으면
 				
-				if(file.createNewFile()) { //  
-					System.out.println(file.getName()+"파일이 생성되었습니다");
+				if(file.createNewFile()) { // 파일 생성이 성공한 경우
+					System.out.println(file.getName() + " 파일이 생성 되었습니다.");
 				}
+				
 			}
-		} catch(IOException e){
-			e.printStackTrace();  // 예외 정보 + 예외가 발생한 위치까지의 메서드 추적
+			
+			
+		} catch(IOException e) {
+			e.printStackTrace(); // 예외 정보 + 예외가 발생한 위치까지의 메서드 추적
+			
 		}
+		
 	}
+	
 	
 	/**
 	 * File 클래스 제공 메서드 활용하기
 	 */
 	public void method3() {
+		
 		// File 객체 생성
 		File directory = new File("\\workspace\\02_Java\\12_io");
 		
-		//지정된 디렉토리에 있는 모든 파일/디렉토리를 File[] 형태로 얻어오기
+		// 지정된 디렉토리에 있는 모든 파일/디렉토리를 File[] 형태로 얻어오기
 		File[] files = directory.listFiles();
 		
-		// 향상된 for문 -> for( 요소저장변수  :  배열|걸렉션){}
-		for(File f : files) {
-			//File.toString() : 지정된 파일/디렉토리의 절대경로
-			//System.out.println(f.toString());
+		// 향상된 for문 ->  for( 요소 저장 변수  :  배열|컬렉션 ) {}
+		for( File f : files ) {
 			
-			//파일명
+			// String File.toString() : 지정된 파일/디렉토리의 절대경로
+			// System.out.println( f.toString() );
+			
+			// 파일명
 			String fileName = f.getName();
 			
 			// 마지막으로 수정한 날짜
 			long lastModified = f.lastModified();
-			//System.out.println(lastModified);
+			//System.out.println(lastModified); // 1708388653187
 			
-			//java.text.SimpleDateFormat : 간단히 날짜 형식을 지정할 수 있는 객체
+			// java.text.SimpleDateFormat : 간단히 날짜 형식을 지정할 수 있는 객체
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a h:mm");
-									// 2024-02-22 오후 2:14
+												// 2024-02-22 오후 2:14
+			
 			// String SimpleDateFormat.format(long) : 
-			// 매개변수 long 값을 지정된 패턴 형식으로 변화하여
-			// 문자열 형태로 반환
+			//   매개변수 long 값을 지정된 패턴 형식으로 변화하여
+			//   문자열 형태로 반환
 			String date = sdf.format(lastModified);
 			
 			//System.out.println(date);
 			
+			
 			// 파일 유형
 			String type = null;
 			if(f.isFile()) type = "파일";
-			else           type = "폴더";
+			else 			type = "폴더";
+			
 			
 			// 파일 크기(byte)
 			String size = f.length() + "B";
-			if(f.isDirectory()) size = ""; //폴더면 ""변경
+			if(f.isDirectory()) size = ""; // 폴더면 "" 변경
 			
-			String result = String.format("%-20s %-20s %-5s %10s", fileName, date, type, size);
+			String result = String.format("%-20s %-20s %-5s %10s", 
+							fileName, date, type, size);
+			
 			
 			System.out.println(result);
 			
-			
-			
-			
 		}
 		
-		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
